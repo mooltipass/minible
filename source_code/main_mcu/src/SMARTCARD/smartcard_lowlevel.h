0ab8f95aa761a18c713cac008dba173bac54ffbf
Mathieu <limpkin@limpkin.fr>
2017-11-29 17:20:39 +0100

    Update smartcard_lowlevel.h

--- source_code/main_mcu/src/SMARTCARD/smartcard_lowlevel.h
+++ source_code/main_mcu/src/SMARTCARD/smartcard_lowlevel.h
@@ -17,6 +17,7 @@ typedef enum    {MAN_FUSE = 0, EC2EN_FUSE = 1, ISSUER_FUSE = 2} card_fuse_type_t
 uint8_t* smartcard_lowlevel_read_smc(uint16_t nb_bytes_total_read, uint16_t start_record_index, uint8_t* data_to_receive);
 void smartcard_lowlevel_write_smc(uint16_t start_index_bit, uint16_t nb_bits, uint8_t* data_to_write);
 pin_check_return_te smartcard_lowlevel_validate_code(volatile uint16_t* code);
+void smartcard_lowlevel_erase_application_zone1_nzone2(BOOL zone1_nzone2);
 card_detect_return_te smartcard_lowlevel_first_detect_function(void);
 void smartcard_lowlevel_blow_fuse(card_fuse_type_te fuse_name);
 det_ret_type_te smartcard_lowlevel_is_card_plugged(void);
@@ -27,9 +28,6 @@ void smartcard_lowlevel_set_pgmrst_signals(void);
 void smartcard_lowlevel_hpulse_delay(void);
 void smartcard_lowlevel_clock_pulse(void);
 void smartcard_lowlevel_detect(void);
-
-// old prototypes
-void eraseApplicationZone1NZone2SMC(uint8_t zone1_nzone2);
 
 // Macros
 /*! \fn     isSmartCardAbsent(void)
