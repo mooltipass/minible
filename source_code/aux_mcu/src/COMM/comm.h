51df6439d4cc404ceed292c8f0bc0aa2564cba3a
Miguel A. Borrego <maborrego@ecofriend.cat>
2018-03-24 09:54:59 +0100

    Changed USART Baudrate to 1Mhz, Updated COMM between AUX MCU and MAIN MCU according to project documentation

--- source_code/aux_mcu/src/COMM/comm.h
+++ source_code/aux_mcu/src/COMM/comm.h
@@ -11,9 +11,18 @@
 #include <asf.h>
 
 /* Constant definition */
-#define COMM_MAX_MSG_SIZE   (536U)
-#define COMM_HEADER_SIZE    (4U)
-#define COMM_PAYLOAD_SIZE   (COMM_MAX_MSG_SIZE-COMM_HEADER_SIZE)
+#define COMM_PAYLOAD_SIZE       (536U)
+#define COMM_PAYLOAD_VALID      (true)
+#define COMM_PAYLOAD_NOT_VALID  (false)
+
+/* Message type */
+typedef struct {
+    uint16_t msg_type;
+    uint16_t reserved;
+    uint8_t  payload[COMM_PAYLOAD_SIZE];
+    uint16_t payload_len;
+    uint16_t payload_valid;
+} T_comm_msg;
 
 /* Message Type enum */
 typedef enum comm_msg_type{
@@ -23,9 +32,15 @@ typedef enum comm_msg_type{
     COMM_MSG_FROM_BLE = COMM_MSG_TO_BLE,
 } T_comm_msg_type;
 
+/* Indicate pkt status */
+typedef struct{
+    uint8_t msg_start :1;
+    uint8_t msg_end   :1;
+} T_comm_pkt_status;
+
 void comm_init(void);
 void comm_task(void);
-void comm_process_in_msg(T_comm_msg_type msg_type, uint8_t* buff, uint16_t buff_len);
+void comm_usb_process_in_pkt(T_comm_pkt_status pkt_status, uint8_t* data, uint16_t data_len);
 void comm_process_out_msg(T_comm_msg_type msg_type, uint8_t* buff, uint16_t buff_len);
 
 #endif /* COMM_H_ */
