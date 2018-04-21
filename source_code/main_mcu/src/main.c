81e31f660ec2c3eead1f2384f8f8b4fdfdf12c1e
Mathieu <limpkin@limpkin.fr>
2018-04-21 20:07:50 +0200

    main application offset, bootloader solution

--- source_code/main_mcu/src/main.c
+++ source_code/main_mcu/src/main.c
@@ -16,14 +16,54 @@
 #include "inputs.h"
 #include "main.h"
 #include "dma.h"
+
 /* Our oled & dataflash & dbflash descriptors */
 accelerometer_descriptor_t acc_descriptor = {.sercom_pt = ACC_SERCOM, .cs_pin_group = ACC_nCS_GROUP, .cs_pin_mask = ACC_nCS_MASK, .int_pin_group = ACC_INT_GROUP, .int_pin_mask = ACC_INT_MASK, .evgen_sel = ACC_EV_GEN_SEL, .evgen_channel = ACC_EV_GEN_CHANNEL, .dma_channel = 3};
 sh1122_descriptor_t plat_oled_descriptor = {.sercom_pt = OLED_SERCOM, .dma_trigger_id = OLED_DMA_SERCOM_TX_TRIG, .sh1122_cs_pin_group = OLED_nCS_GROUP, .sh1122_cs_pin_mask = OLED_nCS_MASK, .sh1122_cd_pin_group = OLED_CD_GROUP, .sh1122_cd_pin_mask = OLED_CD_MASK};
 spi_flash_descriptor_t dataflash_descriptor = {.sercom_pt = DATAFLASH_SERCOM, .cs_pin_group = DATAFLASH_nCS_GROUP, .cs_pin_mask = DATAFLASH_nCS_MASK};
 spi_flash_descriptor_t dbflash_descriptor = {.sercom_pt = DBFLASH_SERCOM, .cs_pin_group = DBFLASH_nCS_GROUP, .cs_pin_mask = DBFLASH_nCS_MASK};
 
-
-uint32_t cntt=0;
+/****************************************************************************/
+/* The blob of code below is aimed at facilitating our development process  */
+/* To understand this, you need to know that:                               */
+/* - the Cortex M0 expects the stack pointer address to be at addr 0x0000   */
+/* - the Cortex M0 expects the reset handler address to be at addr 0x0004   */
+/*                                                                          */
+/* So this is what we do:                                                   */
+/* - put the sram base address + something at addr0 using the array below   */
+/* - then put the address of a custom function made to boot the main code   */
+/*                                                                          */
+/* This is made possible by:                                                */
+/* - adding .flash_start_addr=0x0 to linker option                          */
+/* - adding .start_app_function_addr=0x200 (matches the 2nd element in array*/
+/* - adding --undefined=jump_to_application_function to linker option       */
+/* - adding --undefined=jump_to_application_function_addr to linker option  */
+/*                                                                          */
+/* To move the application address, change APP_START_ADDR & .text           */
+/* What I don't understand: why the "+1" in the second array element        */
+/****************************************************************************/
+const uint32_t jump_to_application_function_addr[2] __attribute__((used,section (".flash_start_addr"))) = {HMCRAMC0_ADDR+100,0x200+1};
+void jump_to_application_function(void) __attribute__((used,section (".start_app_function_addr")));
+void jump_to_application_function(void)
+{
+	/* Overwriting the default value of the NVMCTRL.CTRLB.MANW bit (errata reference 13134) */
+	NVMCTRL->CTRLB.bit.MANW = 1;
+	
+	/* Pointer to the Application Section */
+	void (*application_code_entry)(void);
+	
+	/* Rebase the Stack Pointer */
+	__set_MSP(*(uint32_t*)APP_START_ADDR);
+	
+	/* Rebase the vector table base address */
+	SCB->VTOR = ((uint32_t)APP_START_ADDR & SCB_VTOR_TBLOFF_Msk);
+	
+	/* Load the Reset Handler address of the application */
+	application_code_entry = (void (*)(void))(unsigned *)(*(unsigned *)(APP_START_ADDR + 4));
+	
+	/* Jump to user Reset Handler in the application */
+	application_code_entry();
+}
 
 /*! \fn     main_platform_init(void)
 *   \brief  Initialize our platform
@@ -135,8 +175,6 @@ void main_standby_sleep(void)
     
     /* Resume accelerometer processing */
     lis2hh12_sleep_exit_and_dma_arm(&acc_descriptor);
-    
-    cntt = 0;   
 }
 
 /*! \fn     main(void)
@@ -181,6 +219,7 @@ int main(void)
     uint32_t start_time;
     uint32_t end_time;
     uint32_t abc = 0;
+	uint32_t cntt = 0;
     while(1)
     {
         start_time = timer_get_systick();
