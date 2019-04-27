8039b3c8fcf011ee5400735428218a0a99727672
Mathieu <limpkin@limpkin.fr>
2019-04-27 17:02:04 +0200

    Update logic_user.h

--- source_code/main_mcu/src/LOGIC/logic_user.h
+++ source_code/main_mcu/src/LOGIC/logic_user.h
@@ -11,8 +11,12 @@
 #include "comms_hid_msgs.h"
 #include "defines.h"
 
+/* Defines */
+#define CHECK_PASSWORD_TIMER_VAL    4000
+
 /* Prototypes */
 RET_TYPE logic_user_store_credential(cust_char_t* service, cust_char_t* login, cust_char_t* desc, cust_char_t* third, cust_char_t* password);
+RET_TYPE logic_user_check_credential(cust_char_t* service, cust_char_t* login, cust_char_t* password);
 int16_t logic_user_get_credential(cust_char_t* service, cust_char_t* login, hid_message_t* send_msg);
 ret_type_te logic_user_create_new_user(volatile uint16_t* pin_code, uint8_t* provisioned_key);
 void logic_user_init_context(uint8_t user_id);
