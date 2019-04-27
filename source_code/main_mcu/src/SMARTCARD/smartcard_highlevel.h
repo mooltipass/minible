6172346c34d37cfe9d18cb3a3e16c2b81d08cad0
Mathieu <limpkin@limpkin.fr>
2019-04-27 10:01:16 +0200

    Update smartcard_highlevel.h

--- source_code/main_mcu/src/SMARTCARD/smartcard_highlevel.h
+++ source_code/main_mcu/src/SMARTCARD/smartcard_highlevel.h
@@ -33,6 +33,7 @@ void smartcard_highlevel_set_authenticated_readwrite_to_zone1and2(void);
 void smartcard_highlevel_write_security_code(volatile uint16_t* code);
 RET_TYPE smartcard_high_level_transform_blank_card_into_mooltipass(void);
 uint8_t smartcard_highlevel_get_nb_sec_tries_left(void);
+RET_TYPE smartcard_highlevel_write_second_aes_key(uint8_t* buffer);
 RET_TYPE smartcard_highlevel_write_aes_key(uint8_t* buffer);
 uint8_t smartcard_highlevel_get_nb_az2_writes_left(void);
 mooltipass_card_detect_return_te smartcard_highlevel_card_detected_routine(void);
@@ -41,6 +42,7 @@ uint16_t smartcard_highlevel_read_security_code(void);
 void smartcard_highlevel_erase_smartcard(void);
 void smartcard_highlevel_reset_blank_card(void);
 void smartcard_highlevel_read_aes_key(uint8_t* buffer);
+void smartcard_highlevel_read_second_aes_key(uint8_t* buffer);
 void smartcard_highlevel_read_application_zone1(uint8_t* buffer);
 void smartcard_highlevel_write_application_zone1(uint8_t* buffer);
 void smartcard_highlevel_read_application_zone2(uint8_t* buffer);
@@ -69,25 +71,25 @@ void smartcard_highlevel_write_ec2en_fuse(void);
                 SMART CARD MEMORY MAP
 
 Bit Address                 Description                 Bits    Words
-0–15            Fabrication Zone (FZ)                   16      1
-16–79           Issuer Zone (IZ)                        64      4
-80–95           Security Code (SC)                      16      1
-96–111          Security Code Attempts counter (SCAC)   16      1
-112–175         Code Protected Zone (CPZ)               64      4
-176–687         Application Zone 1 (AZ1)                512     32
-688–735         Application Zone 1 Erase Key (EZ1)      48      3
-736–1247        Application Zone 2 (AZ2)                512     32
-1248–1279       Application Zone 2 Erase Key (EZ2)      32      2
-1280–1407       Application Zone 2 Erase Counter (EC2)  128     8
-1408–1423       Memory Test Zone (MTZ)                  16      1
-1424–1439       Manufacturer’s Zone (MFZ)               16      1
-1440–1455       Block Write/Erase                       16      1
-1456–1471       MANUFACTURER’S FUSE                     16      1
+0â€“15            Fabrication Zone (FZ)                   16      1
+16â€“79           Issuer Zone (IZ)                        64      4
+80â€“95           Security Code (SC)                      16      1
+96â€“111          Security Code Attempts counter (SCAC)   16      1
+112â€“175         Code Protected Zone (CPZ)               64      4
+176â€“687         Application Zone 1 (AZ1)                512     32
+688â€“735         Application Zone 1 Erase Key (EZ1)      48      3
+736â€“1247        Application Zone 2 (AZ2)                512     32
+1248â€“1279       Application Zone 2 Erase Key (EZ2)      32      2
+1280â€“1407       Application Zone 2 Erase Counter (EC2)  128     8
+1408â€“1423       Memory Test Zone (MTZ)                  16      1
+1424â€“1439       Manufacturerâ€™s Zone (MFZ)               16      1
+1440â€“1455       Block Write/Erase                       16      1
+1456â€“1471       MANUFACTURERâ€™S FUSE                     16      1
 1529            EC2EN FUSE (Controls use of EC2)        1
 1552 - 1567     ISSUER FUSE                             161
 
 AZ1 composition (bits): 16 reserved + 256 AES key + 240 MTP password
-AZ2 composition (bits): 16 reserved + 496 MTP login
+AZ2 composition (bits): 16 reserved + 256 secondary AES key + 240 MTP login
 
 */
 
