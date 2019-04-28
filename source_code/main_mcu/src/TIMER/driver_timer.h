0dce5502214d19a5d0ab7f31c83f010cdb3c91b9
Mathieu <limpkin@limpkin.fr>
2019-04-28 08:44:52 +0200

    Update driver_timer.h

--- source_code/main_mcu/src/TIMER/driver_timer.h
+++ source_code/main_mcu/src/TIMER/driver_timer.h
@@ -36,6 +36,7 @@ typedef enum {TIMER_EXPIRED = 0, TIMER_RUNNING = 1} timer_flag_te;
 #define DELAYMS_8M(ms)              DELAYTICKS(US_TO_DLYTICKS_8M(ms*1000))                      //uses 20bytes
 
 /* Prototypes */
+void timer_set_calendar(uint16_t year, uint16_t month, uint16_t day, uint16_t hour, uint16_t minute, uint16_t second);
 timer_flag_te timer_has_timer_expired(timer_id_te uid, BOOL clear);
 void timer_start_timer(timer_id_te uid, uint32_t val);
 void timer_get_calendar(calendar_t* calendar_pt);
