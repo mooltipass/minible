ba726c313b4aec4706212c220ecb28cf1d298e55
Mathieu <limpkin@limpkin.fr>
2019-04-30 10:48:34 +0200

    Update logic_smartcard.h

--- source_code/main_mcu/src/LOGIC/logic_smartcard.h
+++ source_code/main_mcu/src/LOGIC/logic_smartcard.h
@@ -30,6 +30,7 @@
 /* Prototypes */
 RET_TYPE logic_smartcard_ask_for_new_pin(volatile uint16_t* new_pin, uint16_t message_id);
 valid_card_det_return_te logic_smartcard_valid_card_unlock(BOOL hash_allow_flag);
+RET_TYPE logic_smartcard_remove_card_and_reauth_user(void);
 RET_TYPE logic_smartcard_user_unlock_process(void);
 RET_TYPE logic_smartcard_handle_inserted(void);
 void logic_smartcard_handle_removed(void);
