2c624942e8e5f181789b4a669d465bf092004b7d
limpkin <limpkin@limpkin.fr>
2019-07-18 22:40:03 +0200

    keyboard report map to match mooltipass mini

--- source_code/aux_mcu_v4/src/LOGIC/logic_bluetooth.c
+++ source_code/aux_mcu_v4/src/LOGIC/logic_bluetooth.c
@@ -327,37 +327,39 @@ static const ble_custom_event_cb_t hid_custom_event_cb =
 /* keyboard report */
 static uint8_t logic_bluetooth_keyb_report_map[] =
 {
-   0x05, 0x01,      /* Usage Page (Generic Desktop)      */
-   0x09, 0x06,      /* Usage (Keyboard)                  */
-   0xA1, 0x01,      /* Collection (Application)          */
-   0x85, 0x01,      /* REPORT ID (1) - MANDATORY         */ 
-   0x05, 0x07,      /* Usage Page (Keyboard)             */
-   0x19, 224,       /* Usage Minimum (224)               */
-   0x29, 231,       /* Usage Maximum (231)               */
-   0x15, 0x00,      /* Logical Minimum (0)               */
-   0x25, 0x01,      /* Logical Maximum (1)               */
-   0x75, 0x01,      /* Report Size (1)                   */
-   0x95, 0x08,      /* Report Count (8)                  */
-   0x81, 0x02,      /* Input (Data, Variable, Absolute)  */
-   0x81, 0x01,      /* Input (Constant)                  */
-   0x19, 0x00,      /* Usage Minimum (0)                 */
-   0x29, 101,       /* Usage Maximum (101)               */
-   0x15, 0x00,      /* Logical Minimum (0)               */
-   0x25, 101,       /* Logical Maximum (101)             */
-   0x75, 0x08,      /* Report Size (8)                   */
-   0x95, 0x06,      /* Report Count (6)                  */
-   0x81, 0x00,      /* Input (Data, Array)               */
-   0x05, 0x08,      /* Usage Page (LED)                  */
-   0x19, 0x01,      /* Usage Minimum (1)                 */
-   0x29, 0x05,      /* Usage Maximum (5)                 */
-   0x15, 0x00,      /* Logical Minimum (0)               */
-   0x25, 0x01,      /* Logical Maximum (1)               */
-   0x75, 0x01,      /* Report Size (1)                   */
-   0x95, 0x05,      /* Report Count (5)                  */
-   0x91, 0x02,      /* Output (Data, Variable, Absolute) */
-   0x95, 0x03,      /* Report Count (3)                  */
-   0x91, 0x01,      /* Output (Constant)                 */
-   0xC0             /* End Collection                    */
+    0x05, 0x01,                         // Usage Page (Generic Desktop),
+    0x09, 0x06,                         // Usage (Keyboard),
+    0xA1, 0x01,                         // Collection (Application),
+    0x85, 0x01,                         // REPORT ID (1) - MANDATORY
+    0x75, 0x01,                         //   Report Size (1),
+    0x95, 0x08,                         //   Report Count (8),
+    0x05, 0x07,                         //   Usage Page (Key Codes),
+    0x19, 0xE0,                         //   Usage Minimum (224),
+    0x29, 0xE7,                         //   Usage Maximum (231),
+    0x15, 0x00,                         //   Logical Minimum (0),
+    0x25, 0x01,                         //   Logical Maximum (1),
+    0x81, 0x02,                         //   Input (Data, Variable, Absolute), ;Modifier byte
+    0x95, 0x01,                         //   Report Count (1),
+    0x75, 0x08,                         //   Report Size (8),
+    0x81, 0x03,                         //   Input (Constant),                 ;Reserved byte
+    0x95, 0x05,                         //   Report Count (5),
+    0x75, 0x01,                         //   Report Size (1),
+    0x05, 0x08,                         //   Usage Page (LEDs),
+    0x19, 0x01,                         //   Usage Minimum (1),
+    0x29, 0x05,                         //   Usage Maximum (5),
+    0x91, 0x02,                         //   Output (Data, Variable, Absolute), ;LED report
+    0x95, 0x01,                         //   Report Count (1),
+    0x75, 0x03,                         //   Report Size (3),
+    0x91, 0x03,                         //   Output (Constant),                 ;LED report padding
+    0x95, 0x06,                         //   Report Count (6),
+    0x75, 0x08,                         //   Report Size (8),
+    0x15, 0x00,                         //   Logical Minimum (0),
+    0x25, 0xff,                         //   Logical Maximum(255), was 0x68 (104) before
+    0x05, 0x07,                         //   Usage Page (Key Codes),
+    0x19, 0x00,                         //   Usage Minimum (0),
+    0x29, 0xe7,                         //   Usage Maximum (231), was 0x68 (104) before
+    0x81, 0x00,                         //   Input (Data, Array),
+    0xc0                                // End Collection
 };
 
 /* raw keyboard report */
