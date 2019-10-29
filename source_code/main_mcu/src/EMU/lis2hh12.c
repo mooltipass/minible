57b11e0f721a000084bcd907be1b77295da0c04f
Andrzej Szombierski <qq@kuku.eu.org>
2019-10-29 23:05:22 +0100

    EMU: return random data from the accelerometer
    Otherwise the RNG returns all-zero

--- source_code/main_mcu/src/EMU/lis2hh12.c
+++ source_code/main_mcu/src/EMU/lis2hh12.c
@@ -1,11 +1,24 @@
 #include "lis2hh12.h"
+#include <unistd.h>
+#include <fcntl.h>
 
-BOOL lis2hh12_check_data_received_flag_and_arm_other_transfer(accelerometer_descriptor_t* descriptor_pt){ return TRUE; }
+static int urandom = -1;
+
+BOOL lis2hh12_check_data_received_flag_and_arm_other_transfer(accelerometer_descriptor_t* descriptor_pt)
+{
+    if(urandom < 0)
+        urandom = open("/dev/urandom", O_RDONLY);
+    read(urandom, &descriptor_pt->fifo_read.acc_data_array, sizeof(descriptor_pt->fifo_read.acc_data_array));
+    return TRUE; 
+}
+
+RET_TYPE lis2hh12_check_presence_and_configure(accelerometer_descriptor_t* descriptor_pt){ return RETURN_OK; }
+/*
 void lis2hh12_send_command(accelerometer_descriptor_t* descriptor_pt, uint8_t* data, uint32_t length){}
 void lis2hh12_manual_acc_data_read(accelerometer_descriptor_t* descriptor_pt, acc_data_t* data_pt){}
-RET_TYPE lis2hh12_check_presence_and_configure(accelerometer_descriptor_t* descriptor_pt){ return RETURN_OK; }
 void lis2hh12_deassert_ncs_and_go_to_sleep(accelerometer_descriptor_t* descriptor_pt){}
 void lis2hh12_sleep_exit_and_dma_arm(accelerometer_descriptor_t* descriptor_pt){}
 int16_t lis2hh12_get_temperature(accelerometer_descriptor_t* descriptor_pt){}
 void lis2hh12_dma_arm(accelerometer_descriptor_t* descriptor_pt){}
 void lis2hh12_reset(accelerometer_descriptor_t* descriptor_pt){}
+*/
