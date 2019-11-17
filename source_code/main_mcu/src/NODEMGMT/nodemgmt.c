7fcbf753172f35e8ba06090e78252fc4c269506c
limpkin <limpkin@limpkin.fr>
2019-11-17 16:55:40 +0100

    GPLV3ing the code

--- source_code/main_mcu/src/NODEMGMT/nodemgmt.c
+++ source_code/main_mcu/src/NODEMGMT/nodemgmt.c
@@ -1,3 +1,19 @@
+/* 
+ * This file is part of the Mooltipass Project (https://github.com/mooltipass).
+ * Copyright (c) 2019 Stephan Mathieu
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 /*!  \file     nodemgmt.c
 *    \brief    Node management library
 *    Created:  14/12/2018
--- source_code/main_mcu/src/SMARTCARD/smartcard_highlevel.h
+++ source_code/main_mcu/src/SMARTCARD/smartcard_highlevel.h
@@ -1,3 +1,19 @@
+/* 
+ * This file is part of the Mooltipass Project (https://github.com/mooltipass).
+ * Copyright (c) 2019 Stephan Mathieu
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 /*!  \file     smartcard_highlevel.h
 *    \brief    High level driver for AT88SC102 smartcard
 *    Created:  29/11/2017
