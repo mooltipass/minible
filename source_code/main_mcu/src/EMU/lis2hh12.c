477559e707fe1ae5dc4524dc6c54004a5625bc83
limpkin <limpkin@limpkin.fr>
2020-03-23 21:10:37 +0100

    emulator accelerometer bug fix

--- source_code/main_mcu/src/EMU/lis2hh12.c
+++ source_code/main_mcu/src/EMU/lis2hh12.c
@@ -1,19 +1,24 @@
 #include "lis2hh12.h"
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
 
 static int urandom = -1;
 
 BOOL lis2hh12_check_data_received_flag_and_arm_other_transfer(accelerometer_descriptor_t* descriptor_pt, BOOL arm_other_transfer)
 {
     (void)arm_other_transfer;
-    if(urandom < 0)
-        urandom = open("/dev/urandom", O_RDONLY);
-    read(urandom, &descriptor_pt->fifo_read.acc_data_array, sizeof(descriptor_pt->fifo_read.acc_data_array));
+    for (uint16_t i = 0; i < MEMBER_ARRAY_SIZE(acc_single_fifo_read_t, acc_data_array); i++)
+    {
+        /* Only fill the lower bytes as the rng uses them */
+        descriptor_pt->fifo_read.acc_data_array[i].acc_x = 100 + (uint16_t)rand() & 0x000F;
+        descriptor_pt->fifo_read.acc_data_array[i].acc_y = 100 + (uint16_t)rand() & 0x000F;
+        descriptor_pt->fifo_read.acc_data_array[i].acc_z = 100 + (uint16_t)rand() & 0x000F;
+    }
     return TRUE; 
 }
 
-RET_TYPE lis2hh12_check_presence_and_configure(accelerometer_descriptor_t* descriptor_pt){ return RETURN_OK; }
+RET_TYPE lis2hh12_check_presence_and_configure(accelerometer_descriptor_t* descriptor_pt){srand ((unsigned int) time (NULL));return RETURN_OK; }
 /*
 void lis2hh12_send_command(accelerometer_descriptor_t* descriptor_pt, uint8_t* data, uint32_t length){}
 void lis2hh12_manual_acc_data_read(accelerometer_descriptor_t* descriptor_pt, acc_data_t* data_pt){}
