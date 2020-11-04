cdcaff2723c8ceb4258b1931f4cd6dbe7a1d4c11
Mathieu <limpkin@limpkin.fr>
2020-11-04 17:26:31 +0100

    Update logic_battery.h

--- source_code/aux_mcu/src/LOGIC/logic_battery.h
+++ source_code/aux_mcu/src/LOGIC/logic_battery.h
@@ -25,7 +25,7 @@ typedef enum    {BAT_ACT_NONE = 0, BAT_ACT_NEW_BAT_LEVEL, BAT_ACT_CHARGE_FAIL, B
 #define LOGIC_BATTERY_CUR_FOR_RECOVERY      50      // ADC value difference between high & low cursense for recovery ramping: 1LSB = 0.5445mA
 #define LOGIC_BATTERY_CUR_FOR_ST_RAMP_END   100     // ADC value difference between high & low cursense to stop the initial ramping: 1LSB = 0.5445mA
 #define LOGIC_BATTERY_CUR_FOR_DANGER_CHARGE 611     // ADC value difference between high & low cursense for forced dangerous charge (to test battery recovery): 1LSB = 0.5445mA
-#define LOGIC_BATTERY_MAX_V_FOR_RECOVERY_CG 2571    // Voltage at which we stop the recovery charge (around 1.4V)
+#define LOGIC_BATTERY_MAX_V_FOR_RECOVERY_CG 2500    // Voltage at which we stop the recovery charge (around 1.3V)
 #define LOGIC_BATTERY_MAX_V_FOR_ST_RAMP     2938    // Voltage at which we consider that something is wrong during initial ramp (around 1.6V)
 #define LOGIC_BATTERY_START_CHARGE_DELAY    100     // Delay before taking the first decision in our charging algorithm
 #define LOGIC_BATTERY_NB_MIN_SLOW_START     60UL    // Number of minutes we keep a low current for slow start charge
